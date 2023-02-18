ec0c4b6acaa0b0cf1e1b0cd2e2365f19ffa04b5f
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-02-18 19:05:38 +0000

    Versión inicial de página de registro añadida

--- javascript/navigation-bar.js
+++ javascript/navigation-bar.js
@@ -8,8 +8,9 @@ const navBar = `
                     <li class="lista-paginas__reservas"><a href="#">Reservas</a></li>
                     <li class="lista-paginas__about-us"><a href="#">Sobre nosotros</a></li>
                     <li class="lista-paginas__contacto"><a href="#">Contacto</a></li>
-                    <li class="lista-paginas__user-icon"><a href="#" class="user-icon"><img
-                        src="/images/user-icon.png" alt="User"></a></li>
+                    <li class="lista-paginas__user-icon"><a href="#" class="user-icon">
+                        <img src="/images/user-icon.png" alt="User"></a>
+                    </li>
                 </ul>
             </nav>
         </div>
