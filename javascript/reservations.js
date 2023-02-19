cca4edf1c68dc0e4628745c1e05b2e72a2371f9f
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-02-19 23:53:16 +0000

    añadido diálogo de confirmación para cancelar reserva

--- javascript/reservations.js
+++ javascript/reservations.js
@@ -1,24 +1,35 @@
 let reservationsJson = `
     [
       {
+        "id": 1,
         "date": "08/02/2023",
         "hour": "14:30",
         "numberOfPeople": "7",
         "name": "Alberto Santana"
       },
       {
+        "id": 2,
         "date": "10/02/2023",
         "hour": "14:30",
         "numberOfPeople": "2",
         "name": "Alberto Santana"
       },
       {
+        "id": 3,
         "date": "07/02/2023",
         "hour": "14:30",
         "numberOfPeople": "5",
         "name": "Carlos Santana"
       },
       {
+        "id": 4,
+        "date": "15/02/2023",
+        "hour": "14:30",
+        "numberOfPeople": "1",
+        "name": "Alberto Santana"
+      },
+      {
+        "id": 4,
         "date": "15/02/2023",
         "hour": "14:30",
         "numberOfPeople": "1",
@@ -29,65 +40,92 @@ let reservationsJson = `
 
 const MAX_RESERVATIONS = 5
 const reservations = JSON.parse(reservationsJson)
+const table = document.querySelector(".reservations-table")
+const cancelButton = document.querySelector(".cancel-res-button")
+const newResButton = document.querySelector(".new-res-button")
+const confirmationDialog = document.querySelector(".confirmation-dialog")
+const reservationToDelete = document.querySelector("#reservation-to-delete")
+const dialogButtons = document.querySelectorAll(".dialog-button")
 
-function emptyRow() {
-    return `<td colspan="4">Puede realizar ${MAX_RESERVATIONS-reservations.length} reserva/s más</td>`
-}
-
-function fillTable() {
-    const table = document.querySelector(".reservations-table")
-    table.innerHTML = `
-        <tr className="title-row">
+function tableTitleHTML() {
+    return `
+        <tr class="title-row">
             <th>Fecha</th>
             <th>Hora</th>
             <th>Número de personas</th>
             <th>A nombre de</th>
         </tr>
     `
-    reservations.forEach((reservation) => {
-        table.innerHTML += `
-            <tr>
-                <td>${reservation.date}</td>
-                <td>${reservation.hour}</td>
-                <td>${reservation.numberOfPeople}</td>
-                <td>${reservation.name}</td>
-            </tr>
-        `
-    })
-    for (let i = reservations.length; i < MAX_RESERVATIONS; i++) {
-        table.innerHTML += `
-            <tr class="empty-row">
-                ${emptyRow()}
-            </tr>
-        `
-    }
+}
 
+function emptyRowHTML() {
+    return `
+        <tr class="empty-row">
+            <td colspan="4">Puede realizar ${MAX_RESERVATIONS-reservations.length} reserva/s más</td>
+        </tr>
+    `
+}
+
+function reservationHTML(reservation) {
+    return `
+        <tr id="reservation${reservation.id}">
+            <td>${reservation.date}</td>
+            <td>${reservation.hour}</td>
+            <td>${reservation.numberOfPeople}</td>
+            <td>${reservation.name}</td>
+        </tr>
+    `
+}
+
+function checkButtons() {
+    const isRowSelected = document.querySelectorAll(".selected-row").length > 0
+    cancelButton.disabled = !isRowSelected || confirmationDialog.style.display !== "none";
+    newResButton.disabled = reservations.length === MAX_RESERVATIONS;
+}
+
+function fillTable() {
+    table.innerHTML = tableTitleHTML()
+    reservations.forEach((reservation) => table.innerHTML += reservationHTML(reservation))
+    for (let i = reservations.length; i < MAX_RESERVATIONS; i++) table.innerHTML += emptyRowHTML()
+
+    // Rows selection
     const rows = document.querySelectorAll(".reservations-table tr:not(.title-row):not(.empty-row)")
     rows.forEach((row) => {
         row.onclick = () => {
             rows.forEach((r) => {
-                if (r !== row) {
-                    r.classList.remove("selected-row")
-                }
+                if (r !== row) r.classList.remove("selected-row")
             })
             row.classList.toggle("selected-row")
+            checkButtons()
         }
     })
+    checkButtons()
 }
 
 fillTable()
 
-const cancelButton = document.querySelector(".cancel-res-button")
+// Cancel button
 cancelButton.onclick = () => {
     const selectedRow = document.querySelector(".selected-row")
-    const date = selectedRow.childNodes[1].firstChild.textContent
-    const hour = selectedRow.childNodes[3].firstChild.textContent
-    console.log(selectedRow.childNodes)
     for (let i=0; i<reservations.length; i++) {
-        if (reservations[i].date === date && reservations[i].hour === hour) {
+        if (selectedRow.id === "reservation" + reservations[i].id) {
+            showConfirmationDialog(reservations[i])
             reservations.splice(i, 1)
+            if (reservations.length === 0) cancelButton.disabled = true
             fillTable()
             break
         }
+    }
+    checkButtons()
+}
+
+function showConfirmationDialog(reservation) {
+    reservationToDelete.innerHTML = reservationHTML(reservation)
+    confirmationDialog.style.display = "block"
+}
+
+for (const button of dialogButtons) {
+    button.onclick = () => {
+        confirmationDialog.style.display = "none"
     }
 }
