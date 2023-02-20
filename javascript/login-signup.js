869bfda7dc0fb5175be41d8c4e193ac75894b0c5
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-02-20 17:59:22 +0000

    campos del formulario alineados

--- javascript/login-signup.js
+++ javascript/login-signup.js
@@ -1,4 +1,4 @@
-const signUpPageJson = `
+const signUpPageContent =
     {
         "mainTitle": "Regístrese para poder realizar su primera reserva",
         "formFields": [
@@ -26,9 +26,9 @@ const signUpPageJson = `
         "already": "Si ya se ha registrado, ", 
         "alreadyLink": "INICIE SESIÓN AQUÍ"
     }
-`
 
-const logInPageJson = `
+
+const logInPageContent =
     {
         "mainTitle": "Inicie sesión para poder ver sus reservas",
         "formFields": [
@@ -50,45 +50,41 @@ const logInPageJson = `
         "already": "Si aún no se ha registrado, ", 
         "alreadyLink": "REGÍSTRESE AQUÍ"
     }
-`
 
-const signUpPageContent = JSON.parse(signUpPageJson)
-const logInPageContent = JSON.parse(logInPageJson)
 
-const title = document.querySelector("#main-title")
-const form = document.querySelector("#user-form")
+const title = document.querySelector(".main-title")
+const formLabels = document.querySelector(".labels-container")
+const formInputs = document.querySelector(".inputs-container")
+const cancelButton = document.querySelector(".cancel-button")
+const submitButton = document.querySelector(".submit-button")
 const alreadyDiv = document.querySelector(".already-div")
 
 function loadPage(pageContent) {
     title.innerHTML = pageContent.mainTitle
-    form.innerHTML = ""
+    formLabels.innerHTML = ""
+    formInputs.innerHTML = ""
     for (const field of pageContent.formFields) {
-        form.innerHTML += `
-            <div>
-                <label for="${field.id}">${field.label}</label>
-                <input id="${field.id}" type="${field.type}" name="${field.name}" required>
-            </div>
-        `
+        formLabels.innerHTML += `<label for="${field.id}">${field.label}</label>`
+        formInputs.innerHTML += `<input id="${field.id}" type="${field.type}" name="${field.name}" required>`
     }
-    form.innerHTML += `
-        <button class="cancel-button" type="reset">${pageContent.cancelButtonLabel}</button>
-        <button class="submit-button" type="submit">${pageContent.confirmButtonLabel}</button>
-    `
+    cancelButton.innerHTML = pageContent.cancelButtonLabel
+    submitButton.innerHTML = pageContent.confirmButtonLabel
+
     alreadyDiv.innerHTML = `
         <p class="already-paragraph">${pageContent.already}</p>
         <a href="#" id="go-to-login-signup" onclick="changePage()"><b>${pageContent.alreadyLink}</b></a>
     `
 }
 
-let page = "login"
+let currentPage = "login"
 loadPage(logInPageContent)
 
 function changePage() {
-    if (page === "login") {
-        page = "signup"
+    if (currentPage === "login") {
+        currentPage = "signup"
         return loadPage(signUpPageContent)
     }    
-    page = "login"
+    currentPage = "login"
     loadPage(logInPageContent)
 }
 
