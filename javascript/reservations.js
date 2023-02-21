10214859fd35e977fa04ca43a91cb3376d55e13f
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-02-21 17:54:23 +0000

    creado lgged-in-reservations-template.html

--- javascript/reservations.js
+++ javascript/reservations.js
@@ -1,4 +1,19 @@
-let reservationsJson = `
+w3IncludeHTML(loadPage)
+
+let reservationsPageContent =
+    {
+        "title": "Reservas",
+        "tableHeaders": [ "Fecha", "Hora", "Número de personas", "A nombre de" ],
+        "availableResLabel": "Puede realizar $ reserva/s más",
+        "backButtonLabel": "Atrás",
+        "cancelResButtonLabel": "Cancelar reserva",
+        "newResButtonLabel": "Realizar nueva reserva",
+        "selectResAlertMessage": "Seleccione una reserva para poder cancelarla",
+        "confirmCancelMessage": "¿Seguro que desea eliminar la reserva seleccionada?",
+        "maxResAlertMessage": "Ha alcanzado el máximo de reservas"
+    }
+
+let reservations =
     [
       {
         "id": 1,
@@ -27,32 +42,19 @@ let reservationsJson = `
         "hour": "14:30",
         "numberOfPeople": "1",
         "name": "Alberto Santana"
-      },
-      {
-        "id": 5,
-        "date": "15/02/2023",
-        "hour": "14:30",
-        "numberOfPeople": "1",
-        "name": "Alberto Santana"
       }
     ]
-`
 
 const MAX_RESERVATIONS = 5
 const NEW_RESERVATION_HTML = "new-reservation.html"
 
-const reservations = JSON.parse(reservationsJson)
-const table = document.querySelector(".reservations-table")
-const cancelButton = document.querySelector(".cancel-res-button")
-const newResButton = document.querySelector(".new-res-button")
-
 function tableTitleHTML() {
     return `
         <tr class="title-row">
-            <th>Fecha</th>
-            <th>Hora</th>
-            <th>Número de personas</th>
-            <th>A nombre de</th>
+            <th>${reservationsPageContent.tableHeaders[0]}</th>
+            <th>${reservationsPageContent.tableHeaders[1]}</th>
+            <th>${reservationsPageContent.tableHeaders[2]}</th>
+            <th>${reservationsPageContent.tableHeaders[3]}</th>
         </tr>
     `
 }
@@ -60,7 +62,8 @@ function tableTitleHTML() {
 function emptyRowHTML() {
     return `
         <tr class="empty-row">
-            <td colspan="4">Puede realizar ${MAX_RESERVATIONS-reservations.length} reserva/s más</td>
+            <td colspan="4">${reservationsPageContent.availableResLabel
+                .replace("$", (MAX_RESERVATIONS-reservations.length).toString())}</td>
         </tr>
     `
 }
@@ -76,7 +79,7 @@ function reservationHTML(reservation) {
     `
 }
 
-function fillTable() {
+function fillTable(table) {
     table.innerHTML = tableTitleHTML()
     reservations.forEach((reservation) => table.innerHTML += reservationHTML(reservation))
     for (let i = reservations.length; i < MAX_RESERVATIONS; i++) table.innerHTML += emptyRowHTML()
@@ -93,29 +96,38 @@ function fillTable() {
     })
 }
 
-fillTable()
+function loadPage() {
+    const table = document.querySelector(".reservations-table")
+    const backButton = document.querySelector(".back-button")
+    const cancelButton = document.querySelector(".cancel-res-button")
+    const newResButton = document.querySelector(".new-res-button")
 
-// Cancel button
-cancelButton.onclick = () => {
-    const selectedRow = document.querySelector(".selected-row")
-    cancelOnClick(selectedRow, reservations)
+    fillTable(table)
+
+    backButton.innerHTML = reservationsPageContent.backButtonLabel
+
+    cancelButton.innerHTML = reservationsPageContent.cancelResButtonLabel
+    cancelButton.onclick = () => {
+        const selectedRow = document.querySelector(".selected-row")
+        cancelOnClick(selectedRow, table)
+    }
+
+    newResButton.innerHTML = reservationsPageContent.newResButtonLabel
+    newResButton.onclick = () => {
+        if (reservations.length >= MAX_RESERVATIONS) return alert(reservationsPageContent.maxResAlertMessage)
+        window.location.href = NEW_RESERVATION_HTML
+    }
 }
 
-function cancelOnClick(selectedRow, reservations) {
-    if (selectedRow === null) return alert("Seleccione una reserva para poder cancelarla")
+function cancelOnClick(selectedRow, table) {
+    if (selectedRow === null) return alert(reservationsPageContent.selectResAlertMessage)
     for (let i=0; i<reservations.length; i++) {
         if (selectedRow.id === "reservation" + reservations[i].id) {
-            if (confirm("¿Seguro que desea eliminar la reserva seleccionada?")) {
+            if (confirm(reservationsPageContent.confirmCancelMessage)) {
                 reservations.splice(i, 1)
-                fillTable()
+                fillTable(table)
             } 
             break
         }
     }
-}
-
-newResButton.onclick = () => {
-    const selectedRow = document.querySelector(".selected-row")
-    if (reservations.length >= MAX_RESERVATIONS) return alert("Ha alcanzado el máximo de reservas")
-    window.location.href = NEW_RESERVATION_HTML
 }
